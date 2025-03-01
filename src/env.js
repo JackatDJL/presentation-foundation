4eda252ca4518e74effef2dcd26a5f07f9b5d43f
DJL - Jack <71508487+JackatDJL@users.noreply.github.com>
2025-03-01 08:24:38 +0000

    Update layout metadata and enhance environment variable handling

--- src/env.js
+++ src/env.js
@@ -1,44 +1,52 @@
 import { createEnv } from "@t3-oss/env-nextjs";
+import { config } from "dotenv";
 import { z } from "zod";
 
+config({path: ".env.local"});
+
 export const env = createEnv({
-  /**
-   * Specify your server-side environment variables schema here. This way you can ensure the app
-   * isn't built with invalid env vars.
-   */
   server: {
     DATABASE_URL: z.string().url(),
-    NODE_ENV: z
-      .enum(["development", "test", "production"])
-      .default("development"),
+    DATABASE_URL_UNPOOLED: z.string().url(),
+    PGDATABASE: z.string(),
+    PGHOST: z.string(),
+    PGHOST_UNPOOLED: z.string(),
+    PGPASSWORD: z.string(),
+    PGUSER: z.string(),
+    POSTGRES_DATABASE: z.string(),
+    POSTGRES_HOST: z.string(),
+    POSTGRES_PASSWORD: z.string(),
+    POSTGRES_PRISMA_URL: z.string().url(),
+    POSTGRES_URL: z.string().url(),
+    POSTGRES_URL_NON_POOLING: z.string().url(),
+    POSTGRES_URL_NO_SSL: z.string().url(),
+    POSTGRES_USER: z.string(),
+    UPLOADTHING_TOKEN: z.string(),
+    NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
   },
-
-  /**
-   * Specify your client-side environment variables schema here. This way you can ensure the app
-   * isn't built with invalid env vars. To expose them to the client, prefix them with
-   * `NEXT_PUBLIC_`.
-   */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
   },
-
-  /**
-   * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
-   * middlewares) or client-side so we need to destruct manually.
-   */
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE_URL_UNPOOLED: process.env.DATABASE_URL_UNPOOLED,
+    PGDATABASE: process.env.PGDATABASE,
+    PGHOST: process.env.PGHOST,
+    PGHOST_UNPOOLED: process.env.PGHOST_UNPOOLED,
+    PGPASSWORD: process.env.PGPASSWORD,
+    PGUSER: process.env.PGUSER,
+    POSTGRES_DATABASE: process.env.POSTGRES_DATABASE,
+    POSTGRES_HOST: process.env.POSTGRES_HOST,
+    POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+    POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
+    POSTGRES_URL: process.env.POSTGRES_URL,
+    POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING,
+    POSTGRES_URL_NO_SSL: process.env.POSTGRES_URL_NO_SSL,
+    POSTGRES_USER: process.env.POSTGRES_USER,
+    UPLOADTHING_TOKEN: process.env.UPLOADTHING_TOKEN,
     NODE_ENV: process.env.NODE_ENV,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
-  /**
-   * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
-   * useful for Docker builds.
-   */
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
-  /**
-   * Makes it so that empty strings are treated as undefined. `SOME_VAR: z.string()` and
-   * `SOME_VAR=''` will throw an error.
-   */
   emptyStringAsUndefined: true,
 });
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,16 +1,7 @@
 // src/db.ts
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
-import { config, type DotenvConfigOutput } from "dotenv";
+import { env } from "~/env";
 
-const result: DotenvConfigOutput = config({ path: ".env.local" }); // or .env.local
-if (result.error) {
-  throw result.error;
-}
-const databaseUrl = process.env.DATABASE_URL;
-if (!databaseUrl) {
-  throw new Error("DATABASE_URL environment variable is not set");
-}
-
-const sql = neon(databaseUrl);
+const sql = neon(env.DATABASE_URL);
 export const db = drizzle(sql);
