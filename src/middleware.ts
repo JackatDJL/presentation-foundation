e088b6c751619866748a0b8576bec175944c7a2c
JackatDJL <71508487+JackatDJL@users.noreply.github.com>
2025-06-27 21:23:24 +0200

    Worked on the idea on how ill rebuild my router

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import { clerkMiddleware, createRouteMatcher, currentUser } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { type NextRequest, NextResponse } from "next/server";
 
 // Future Routing Structure
@@ -12,8 +12,76 @@ import { type NextRequest, NextResponse } from "next/server";
 // pr.djl.foundation/{example-user}/{shortname} = presentation view for presentation "shortname" - Free Tier
 // pr.djl.foundation/!{shortname} = presentation view for presentation "shortname" - Pro Tier
 // pr.djl.foundation/{example-user} = user profile w/public presentations - intelligent routing (e.g. if auth().user == example-user redirect to /)
-// example-org.pr.djl.foundation = organisation profile (if wanted) (intelligent routing for profile / org home)
-// example-org.pr.djl.foundation/{shortname} = organisation presentation view for presentation "shortname"
+// example-org.pr.djl.foundation = organisation profile (if wanted) (intelligent routing for profile / org home) // Alias Org-Root
+// example-org.pr.djl.foundation/{shortname} = organisation presentation view for presentation "shortname"+
+
+// Integration für Custom Domains (Pro Org)
+// e.g. subdomain.yourdomain.com (example: keynotes.hackclub-stade.de) // Need ideas on how to implement, maybe just a cname to pr.djl.foundation works? or does double cnames not work? e.g. customdomain.com -> pr.djl.foundation -> cname.vercel.com. or smth
+
+// Needed Route List
+/**
+ * - (auth) // Same as Always // Below new stuff
+ * - - /org/create // Root
+ * - - /org/settings // Root / Org Clerk Profile / if on custom subdomain or domain /settings gets rewritten to /org/settings
+ * - - /profile/select // Root / Org Clerk Selector
+ * - (legal) // Same as Always
+ * - (management) // Only Root / Org Root
+ * - - /manage // Root / Org Root / needs revamped ui
+ * - - /edit/{shortname} // Root / Org Root
+ * - - /create // Root / Org Root
+ * - - (hidden routing)
+ * - - - /_internal/hero // Only Rootdomain!
+ * - - - /_internal/home // Rootdomain / Org Rootdomain
+ * - - - /_internal/view/[shortname] // Also gets rewritten from /[username]/[shortname] on free tier as well as subdomains and orgs subdomains /[shortname]
+ * - - - /_internal/layout.ts // File that checks for a cookie or header from middleware to only pass through to the only rewritten targeted routes if the middleware actualy wanted that to happen
+
+* - / // the root page.tsx // but thanks to middleware inaccessible
+ */
+
+// Route List: (current / old way)
+/**
+ * - (auth) // Mostly only root routes
+ * - - /sign-in // Everywhere
+ * - - /sign-up // Root
+ * - - /pricing // Root
+ * - - /waitlist // Root
+ * - - /profile // Root
+ * - (legal) // Root
+ * - - /terms // Root
+ * - - /privacy // Root
+ * - (management) // Only Root / Org Root
+ * - - /manage
+ * - - /edit/{shortname}
+ * - - /create
+ * - / // Presentation View, Hero, Home and more...
+ */
+
+// Forbidden User and Shortnames:
+/**
+ * - /sign-in
+ * - /sign-up
+ * - /pricing
+ * - /waitlist
+ * - /profile
+ * - /terms
+ * - /privacy
+ * - /manage
+ * - /edit
+ * - /create
+ * - /org
+ * - /settings
+ * - /select
+ * - /hero
+ * - /home
+ * - /view
+ * - /layout
+ * - /_internal
+ * - /_next
+ * - /api
+ * - /trpc
+ */
+
+const forbiddenNames = [];
 
 const isAuth = createRouteMatcher([
   "/sign-in(.*)",
@@ -37,10 +105,7 @@ function isOrg(req: NextRequest) {
   return hostname.includes(".pr.djl.foundation");
 }
 
-
-
 // returns "user-profile" | "presentation-view" | "org-profile" | "hero" | "home"
-
 
 const isRootRoute = createRouteMatcher(["/"]);
 
